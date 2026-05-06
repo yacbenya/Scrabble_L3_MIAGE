e86a9c375857bd38c8dff17450e8ca67aa0ef432
yacbenya <ycn.benya@gmail.com>
2026-05-06 13:28:04 +0200

    Delete dictionnaire/telecharger.sh

--- dictionnaire/telecharger.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-echo "Telechargement d'une liste complete de mots francais..."
-curl -fL -o mots.txt "https://raw.githubusercontent.com/lorenbrichter/Words/master/Words/fr.txt"
-echo "Liste telechargee dans mots_complet.txt."
-echo "Pour l'utiliser, renommez ou copiez le fichier en mots.txt."
